e4b4f44ad0c69ca4365b7f997da550aaedb901c0
unix <development@dragontale.de>
2016-02-24 23:27:55 +0100

    Update Reiterferien for 2016.

--- Reiterferien.php
+++ Reiterferien.php
@@ -1,6 +1,6 @@
 <?php
 $year = now('year');
-$costs = 330;
+$costs = 340;
 ?>
 
 <table class="shadow_blue_">
@@ -11,11 +11,13 @@ $costs = 330;
 <tbody><tr>
 <td colspan="3">Verl&auml;ngerung oder Verk&uuml;rzung der Aufenthalte nach Absprache m&ouml;glich.
 Weitere Termine auf Anfrage! 
-<span class="anno">Start Anmeldung: <span class="forest">Januar <?php echo $year ?></span>!<span style="display:block">
+<span class="anno">Start Anmeldung: <span class="forest">Januar <?php echo $year ?></span>!<!--<span style="display:block">
     Zus&auml;tzliche M&uuml;llabfuhrgeb&uuml;hren vom Land f&uuml;r Ferienbetrieb eingefordert, trotz eindeutig vernachl&auml;ssigbarem M&uuml;llanfall unserer ordentlichen Ferieng&auml;ste! :/
-    <!--
+    
+    
     Dieses Jahr keine Preiserh&ouml;hung! Viel Spa&szlig;!
-    --></span>
+    </span>
+    -->
 </span>
 </td></tr>
 <tr><td colspan="3"><!-- ____________________________ --><hr class="trenn1" /></td></tr>
@@ -25,13 +27,13 @@ Weitere Termine auf Anfrage!
 <strong>Osterferien</strong></td><td></td></tr>
 <tr>
 <td>1. Woche:</td>
-<td>Mo, 30.03. - Do, 02.04.<?php echo $year ?></td>
-<td><strong>***  &euro; </strong> Kreisjugendring</td>
+<td>Mo, 21.03. - Do, 24.03.<?php echo $year ?></td>
+<td><strong><?php echo $costs ?> &euro;<a class="" href="./.anmeldeformular.pdf"> anmelden</a></strong></td>
 </tr>
 
 <tr>
 <td>2. Woche:</td>
-<td>Do, 09.04. - So, 12.04.<?php echo $year ?></td>
+<td>Di, 29.03. - So, 01.04.<?php echo $year ?></td>
 <td><strong>290 &euro;</strong> <a href="./.anmeldeformular.pdf"> anmelden</a></td>
 </tr>
 
@@ -44,15 +46,15 @@ Weitere Termine auf Anfrage!
 <strong>Pfingstferien</strong></td></tr>
 <tr>
 <td>1. Woche</td>
-<td>So, 24.05. - Do, 28.05.<?php echo $year ?><br />
+<td>So, 15.05. - Do, 19.05.<?php echo $year ?><br />
 <span style="font-size:0.8em;color:#888;">(Verl&auml;ngerung m&ouml;glich)</span></td>
 <td><strong><?php echo $costs ?> &euro;<a class="" href="./.anmeldeformular.pdf"> anmelden</a><br />
 <span style="color:#888; font-size:0.8em;">+ 55 &euro; / Tag Verl&auml;ngerung</span></strong></td>
 </tr>
 <tr>
 <td>2. Woche</td>
-<td>Mo, 01.06. - Fr, 05.06.<?php echo $year ?></td>
-<td><strong><?php echo $costs ?> &euro;<a class="" href="./.anmeldeformular.pdf"> anmelden</a></strong></td>
+<td>Mo, 23.05. - Fr, 27.05.<?php echo $year ?></td>
+<td><strong>***  &euro; </strong> Kreisjugendring</td>
 </tr>
 
 
@@ -64,33 +66,33 @@ Weitere Termine auf Anfrage!
 <strong>Sommerferien</strong></td></tr>
 <tr>
 <td><span class="">1. Woche:</span></td>
-<td>Mo, 03.08. - Fr, 07.08.<?php echo $year ?></td>
+<td>Mo, 01.08. - Fr, 05.08.<?php echo $year ?></td>
 <td><strong><?php echo $costs ?> &euro;</strong> <a href="./.anmeldeformular.pdf"> anmelden</a></td></tr>
 
 <tr>
 <td><span class="">2. Woche:</span></td>
-<td>10.08. - 14.08.<?php echo $year ?></td>
+<td>08.08. - 12.08.<?php echo $year ?></td>
 <td><strong><?php echo $costs ?> &euro;</strong> <a href="./.anmeldeformular.pdf"> anmelden</a></td></tr>
 
 <tr>
 <td><span class="">3. Woche:</span></td>
-<td>17.08. - 21.08.<?php echo $year ?></td>
+<td>15.08. - 19.08.<?php echo $year ?></td>
 <td><strong><?php echo $costs ?> &euro;</strong><a href="./.anmeldeformular.pdf"> anmelden</a></td></tr>
 
 <tr>
 <td><span class="">4. Woche:</span></td>
-<td>24.08. - 28.08.<?php echo $year ?></td>
+<td>22.08. - 26.08.<?php echo $year ?></td>
 <td><strong><?php echo $costs ?> &euro;</strong><!-- color: #00e000; --> <a href="./.anmeldeformular.pdf"> anmelden</a></td></tr>
 
 <tr>
 <td>5. Woche:</td>
-<td>31.08. - 04.09.<?php echo $year ?></td>
-<td><strong><?php echo $costs ?> &euro; </strong><a href="./.anmeldeformular.pdf"> auf Anfrage</a></td></tr>
+<td>29.08. - 02.09.<?php echo $year ?></td>
+<td><strong><?php echo $costs ?> &euro; </strong><!--<a href="./.anmeldeformular.pdf">-->auf Anfrage<!--</a>--></td></tr>
 
 <tr>
 <td>6. Woche:</td>
-<td>07.09. - 11.09.<?php echo $year ?></td>
-<td><strong><?php echo $costs ?> &euro; </strong><a href="./.anmeldeformular.pdf"> auf Anfrage</a></td></tr>
+<td>05.09. - 09.09.<?php echo $year ?></td>
+<td><strong><?php echo $costs ?> &euro; </strong><!--<a href="./.anmeldeformular.pdf">--> auf Anfrage<!--</a>--></td></tr>
 
 
 <!--
@@ -108,7 +110,7 @@ Weitere Termine auf Anfrage!
 <strong>Herbstferien</strong></td></tr>
 <tr>
 <td>1. Woche</td>
-<td>Mo, 02.11. - Fr, 06.11.<?php echo $year ?></td>
+<td>Mo, 31.10. - Fr, 04.11.<?php echo $year ?></td>
 <td><strong><?php echo $costs ?> &euro;</strong> <a href="./.anmeldeformular.pdf"> anmelden</a></td></tr>
 
 <tr><td colspan="3"><!-- ____________________________ --><hr class="trenn1" /></td></tr>
@@ -137,7 +139,7 @@ Weitere Termine auf Anfrage!
 $holidaysParsed = array('row_label', 'Winter', 'Ostern', 'Pfingsten', 'Sommer', 'Herbst', 'Weihnachten');
 $holidaysWanted = array('Ostern', 'Pfingsten', 'Sommer', 'Herbst');
 // PARSE
-$url = "http://www.schulferien.org/Schulferien_nach_Jahren/2015/schulferien_2015.html";
+$url = "http://www.schulferien.org/Schulferien_nach_Jahren/$year/schulferien_$year.html";
 $handle = null;
 // save webpage to file using curl: http://codular.com/curl-with-php
 /*
@@ -199,19 +201,23 @@ $config = array(
     'input-xml'    => false, #true,
 #    'add-xml-decl' => true,
 );
+//$html = strip_tags_content($html, '<br><br/><br /><span></span><div></div><p></p><a></a><ul></ul><ol></ol><li></li><table><tbody></tbody></table><tr></tr><td></td><h1></h1><h2></h2><h3></h3>');
+$html = strip_tags_content($html, '<script></script>', true);
 $tidy->ParseString($html, $config);
 $tidy->cleanRepair();
 file_put_contents($cache_filename, $year.''.$tidy);
 $html = $tidy;
 
+//$html = preg_replace('~//\s*?<!\[CDATA\[\s*|\s*//\]\]>~', '', $html); // see http://stackoverflow.com/questions/3466035/how-to-skip-invalid-characters-in-xml-file-using-php
+// and http://stackoverflow.com/questions/9855055/how-to-ignore-cdata-tags  
+//$html = stripInvalidXml('' . $html);
+//$html = sanitize_for_xml(''.$html);
 
 $dom = new DOMDocument('1.0', 'utf-8');
+
 #echo $dom->saveXML();
 //echo $tidy;
 #$dom->load($tidy); //loadXML | loadHTML both throw namespace warning when undefined namespace is encountered. load() filters these namespaces out.
-
-
-$dom = new DOMDocument('1.0', 'utf-8');
 libxml_use_internal_errors(true);
 $is_loaded = $dom->loadHTML($html);#, LIBXML_NOCDATA | LIBXML_NOWARNING);# | LIBXML_NOERROR);
 $error = libxml_get_last_error();
@@ -257,7 +263,7 @@ while (--$nodeList_index > -1)
     #echo '================<br />'."\r\n";
     #echo 'Examining row ' . $nodeList_index .'<br />'."\r\n";
     $row = $nodeList->item($nodeList_index);
-    #echo $row->nodeValue . ' vs. ' .$row->textContent; <-- both equal!
+    #echo $row->nodeValue . ' vs. ' .$row->textContent; # <-- both equal!
 /*
     $cols = $row->childNodes;
     $cols_index = $cols->length;
@@ -320,7 +326,8 @@ while (++$index < $cols_count)
 
     // PARSE DATE:
     #echo $col->ownerDocument->saveHTML($col);
-    $holidays_date_interval = trim(get_text_from_node($col));
+    $holidays_date_interval = trim(get_text_from_node_recursively($col));
+    #$holidays_date_interval = trim(get_text_from_node($col));
     #->textContent;#nodeValue;
     
 
@@ -427,18 +434,20 @@ table hr.trenn1 { display: inline !important; } /* first hr.trenn1 is somehow ge
 /*]]>*/</style>
 <?php
 
-function dom_find_all_text_nodes($node)
+function dom_find_all_text_nodes($node, $nodes)
 {
-    echo 'dom_find_all_text_nodes(): Line:'.__LINE__.'<br />'."\r\n";
-    print_r($node);
-    echo '<br />'."\r\n" . 'XML representation: ' . ($node->ownerDocument->saveXML($node)) . '<br />'."\r\n";
-    if (!isset($node))
-        return array();
+    #echo 'dom_find_all_text_nodes(): Line:'.__LINE__.'<br />'."\r\n";
+    #print_r($node);
+    #echo '<br />'."\r\n" . 'XML representation: ' . ($node->ownerDocument->saveXML($node)) . '<br />'."\r\n";
+    if (!isset($node) || !$node)
+        return;
     if ($node->nodeType == XML_TEXT_NODE)
-        return array($node);
+    {
+        $nodes[] = $node;
+        return;
+    }
 
     //print_r($node);
-    $nodes = array();
     
     $nodeList = $node->childNodes;
     #$nodeList_index = $nodeList->length; <-- Attention: Here the text order is messed up.
@@ -448,33 +457,41 @@ function dom_find_all_text_nodes($node)
         $childNode = $nodeList->item($nodeList_index);
         if ($childNode->nodeType != XML_TEXT_NODE)
         {
-            $child_childTextNodes = dom_find_all_text_nodes($childNode);
-            print_r($child_childTextNodes);
-            $nodes = array_merge($nodes, $child_childTextNodes);
-            #continue;
+            dom_find_all_text_nodes($childNode, $nodes);
         }
         else
-            $nodes[] = $childNode;
+            if (array_search($childNode, $nodes) === false)
+            {
+                #echo '<br />Found text node: ';
+                #print_r($childNode);
+            	$nodes[] = $childNode;
+            }
     }
-    print_r($nodes);
-    return $nodes;
+    #print_r($nodes);
+    #return $nodes;
 }
 function get_text_from_node_recursively($node)
 {
-    echo 'dom_get_text(): Line:'.__LINE__.'<br />'."\r\n";
-    echo $node->ownerDocument->saveHTML($node);
+    return $node->ownerDocument->saveXML($node);
+    if ($node->nodeType == XML_TEXT_NODE)
+        return $node->textContent;
+    if (!$node->hasChildNodes())
+        return '';
+    #echo 'dom_get_text(): Line:'.__LINE__.'<br />'."\r\n";
+    #echo $node->ownerDocument->saveHTML($node);
     $string = "";
-    #$nodes = dom_find_all_text_nodes($node);
+    $nodes = array();
+    dom_find_all_text_nodes($node, $nodes);
     // TODO HOW CONCATENATE TEXT IF MIXED CHILD CONTENT TYPES, e.g. OBJECT TREE and TEXT NODE?
     // Here a workaround:
-    $nodes = dom_find_all_nodes_with_text_nodes_as_children($node);
+    #$nodes = dom_find_all_nodes_with_text_nodes_as_children($node);
     // Then iterate these found elements in the correct order, and remove all elements that are no text nodes. TODO TODO <-- this should work!
     $nodes_count = sizeOf($nodes);
     $nodes_index = -1;
     while (++$nodes_index < $nodes_count) // iteration direction matters!
     {
        $n = $nodes[$nodes_index];
-       #echo $n;
+       #echo '<br/><br />';
        #echo $n->ownerDocument->documentElement->nodeValue;
        #$s = (string)$n;
        #print_r($n);
@@ -483,7 +500,7 @@ function get_text_from_node_recursively($node)
        #$s = $n->nodeValue; // TODO textValue() ?
        #echo 'nodeValue: ' . $s . '<br />'."\r\n";
        #$s = $n->textContent;
-       echo 'textContent: ' . $s . '<br />'."\r\n";
+       #echo 'textContent: ' . $s . '<br />'."\r\n";
        $string .= $s;
     }
     #echo 'string: '.$string."\n";
@@ -520,4 +537,242 @@ function get_text_from_node($n)
         return $string;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * Removes invalid XML
+ *
+ * @access public
+ * @param string $value
+ * @return string
+ */
+function stripInvalidXml($value)
+{
+    $ret = "";
+    $current;
+    if (empty($value)) 
+    {
+        return $ret;
+    }
+
+    $length = strlen($value);
+    for ($i=0; $i < $length; $i++)
+    {
+        $current = ord($value{$i});
+        if (($current == 0x9) ||
+            ($current == 0xA) ||
+            ($current == 0xD) ||
+            (($current >= 0x20) && ($current <= 0xD7FF)) ||
+            (($current >= 0xE000) && ($current <= 0xFFFD)) ||
+            (($current >= 0x10000) && ($current <= 0x10FFFF)))
+        {
+            $ret .= chr($current);
+        }
+        else
+        {
+            $ret .= " ";
+        }
+    }
+    return $ret;
+}
+/*
+$utf_8_range = range(0, 1114111);
+$output = ords_to_utfstring($utf_8_range);
+$sanitized = sanitize_for_xml($output);
+*/
+
+/**
+ * Removes invalid XML
+ *
+ * @access public
+ * @param string $value
+ * @return string
+ */
+function sanitize_for_xml($input) {
+  // Convert input to UTF-8.
+  $old_setting = ini_set('mbstring.substitute_character', '"none"');
+  $input = mb_convert_encoding($input, 'UTF-8', 'auto');
+  ini_set('mbstring.substitute_character', $old_setting);
+
+  // Use fast preg_replace. If failure, use slower chr => int => chr conversion.
+  $output = preg_replace('/[^\x{0009}\x{000a}\x{000d}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}]+/u', '', $input);
+  if (is_null($output)) {
+    // Convert to ints.
+    // Convert ints back into a string.
+    $output = ords_to_utfstring(utfstring_to_ords($input), TRUE);
+  }
+  return $output;
+}
+
+/**
+ * Given a UTF-8 string, output an array of ordinal values.
+ *
+ * @param string $input
+ *   UTF-8 string.
+ * @param string $encoding
+ *   Defaults to UTF-8.
+ *
+ * @return array
+ *   Array of ordinal values representing the input string.
+ */
+function utfstring_to_ords($input, $encoding = 'UTF-8'){
+  // Turn a string of unicode characters into UCS-4BE, which is a Unicode
+  // encoding that stores each character as a 4 byte integer. This accounts for
+  // the "UCS-4"; the "BE" prefix indicates that the integers are stored in
+  // big-endian order. The reason for this encoding is that each character is a
+  // fixed size, making iterating over the string simpler.
+  $input = mb_convert_encoding($input, "UCS-4BE", $encoding);
+
+  // Visit each unicode character.
+  $ords = array();
+  for ($i = 0; $i < mb_strlen($input, "UCS-4BE"); $i++) {
+    // Now we have 4 bytes. Find their total numeric value.
+    $s2 = mb_substr($input, $i, 1, "UCS-4BE");
+    $val = unpack("N", $s2);
+    $ords[] = $val[1];
+  }
+  return $ords;
+}
+
+/**
+ * Given an array of ints representing Unicode chars, outputs a UTF-8 string.
+ *
+ * @param array $ords
+ *   Array of integers representing Unicode characters.
+ * @param bool $scrub_XML
+ *   Set to TRUE to remove non valid XML characters.
+ *
+ * @return string
+ *   UTF-8 String.
+ */
+function ords_to_utfstring($ords, $scrub_XML = FALSE) {
+  $output = '';
+  foreach ($ords as $ord) {
+    // 0: Negative numbers.
+    // 55296 - 57343: Surrogate Range.
+    // 65279: BOM (byte order mark).
+    // 1114111: Out of range.
+    if (   $ord < 0
+        || ($ord >= 0xD800 && $ord <= 0xDFFF)
+        || $ord == 0xFEFF
+        || $ord > 0x10ffff) {
+      // Skip non valid UTF-8 values.
+      continue;
+    }
+    // 9: Anything Below 9.
+    // 11: Vertical Tab.
+    // 12: Form Feed.
+    // 14-31: Unprintable control codes.
+    // 65534, 65535: Unicode noncharacters.
+    elseif ($scrub_XML && (
+               $ord < 0x9
+            || $ord == 0xB
+            || $ord == 0xC
+            || ($ord > 0xD && $ord < 0x20)
+            || $ord == 0xFFFE
+            || $ord == 0xFFFF
+            )) {
+      // Skip non valid XML values.
+      continue;
+    }
+    // 127: 1 Byte char.
+    elseif ( $ord <= 0x007f) {
+      $output .= chr($ord);
+      continue;
+    }
+    // 2047: 2 Byte char.
+    elseif ($ord <= 0x07ff) {
+      $output .= chr(0xc0 | ($ord >> 6));
+      $output .= chr(0x80 | ($ord & 0x003f));
+      continue;
+    }
+    // 65535: 3 Byte char.
+    elseif ($ord <= 0xffff) {
+      $output .= chr(0xe0 | ($ord >> 12));
+      $output .= chr(0x80 | (($ord >> 6) & 0x003f));
+      $output .= chr(0x80 | ($ord & 0x003f));
+      continue;
+    }
+    // 1114111: 4 Byte char.
+    elseif ($ord <= 0x10ffff) {
+      $output .= chr(0xf0 | ($ord >> 18));
+      $output .= chr(0x80 | (($ord >> 12) & 0x3f));
+      $output .= chr(0x80 | (($ord >> 6) & 0x3f));
+      $output .= chr(0x80 | ($ord & 0x3f));
+      continue;
+    }
+  }
+  return $output;
+}
+
+// And to do this on a simple object or array
+
+// Recursive sanitize_for_xml.
+function recursive_sanitize_for_xml(&$input){
+  if (is_null($input) || is_bool($input) || is_numeric($input)) {
+    return;
+  }
+  if (!is_array($input) && !is_object($input)) {
+    $input = sanitize_for_xml($input);
+  }
+  else {
+    foreach ($input as &$value) {
+      recursive_sanitize_for_xml($value);
+    }
+  }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function strip_tags_content($text, $tags = '', $invert = FALSE) {
+
+  preg_match_all('/<(.+?)[\s]*\/?[\s]*>/si', trim($tags), $tags);
+  $tags = array_unique($tags[1]);
+   
+  if(is_array($tags) AND count($tags) > 0) {
+    if($invert == FALSE) {
+      return preg_replace('@<(?!(?:'. implode('|', $tags) .')\b)(\w+)\b.*?>.*?</\1>@si', '', $text);
+    }
+    else {
+      return preg_replace('@<('. implode('|', $tags) .')\b.*?>.*?</\1>@si', '', $text);
+    }
+  }
+  elseif($invert == FALSE) {
+    return preg_replace('@<(\w+)\b.*?>.*?</\1>@si', '', $text);
+  }
+  return $text;
+}
 ?>
+
+
+
