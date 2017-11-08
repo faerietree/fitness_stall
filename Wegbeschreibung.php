151523ad9725763f4f1b80f72f9ed2038cf96696
faerietree <radagast@ciry.at>
2017-11-08 14:53:10 +0100

    Wegbeschreibung: Link to Google maps, show location overview Germany.

--- Wegbeschreibung.php
+++ Wegbeschreibung.php
@@ -1,6 +1,16 @@
 <p>
-    <strong>von Weilheim</strong> --&gt; Schongau &uuml;ber Burggen nach Bernbeuren. In Bernbeuren Richtung Auerberg. Nach dem Rehgehege auf halber H&ouml;he rechts Abzweigung G&uuml;nther-St&ouml;tten. Nach 300 m geradeaus ist Badwerk ausgeschildert. Reststrecke ca. 2 Km.
+    <strong>von Weilheim</strong> &rarr; Schongau &uuml;ber Burggen nach Bernbeuren. In Bernbeuren Richtung Auerberg. Nach dem Rehgehege auf halber H&ouml;he rechts Abzweigung G&uuml;nther-St&ouml;tten. Nach 300 m geradeaus ist Badwerk ausgeschildert. Reststrecke ca. 2 Km.
 </p>
 <p>
-    <strong>von Marktoberdorf</strong> --&gt; St&ouml;tten, in St&ouml;tten Richtung Auerberg &uuml;ber Buchen, Reinharten, Prachtsried bis G&uuml;nther. In G&uuml;nther vor Kapelle links ca. 2 Km noch bis Badwerk.
+    <strong>von Marktoberdorf</strong> &rarr; St&ouml;tten, in St&ouml;tten Richtung Auerberg &uuml;ber Buchen, Reinharten, Prachtsried bis G&uuml;nther. In G&uuml;nther vor Kapelle links ca. 2 Km noch bis Badwerk.
 </p>
+
+
+
+<a href="
+https://www.google.de/maps/place/Badwerk+1,+86975+Bernbeuren/@47.7475336,10.7455189,16z/data=!4m5!3m4!1s0x479c4346f7abdb55:0xe6981e19f375b211!8m2!3d47.747649!4d10.748405
+"
+>
+<img src="images/fitness_stall_wegbeschreibung.jpg" alt="" style="width:100%; max-width: 100%;"/>
+Fitness-Stall auf Google Maps &copy Google
+</a>
