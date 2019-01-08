d561e4db505c367b277be91197b89ec79fccc622
faerietree (ciryat) <radagast@ciry.at>
2019-01-08 14:13:04 +0000

    Tidy up some whitespace.

--- Unsere_Pferde.php
+++ Unsere_Pferde.php
@@ -1,6 +1,6 @@
 <?php
 /*------------------*
- |Unsere_Pferde.php 
+ |Unsere_Pferde.php
  *------------------*/
 if (isset($_SESSION['usr'])) { ?>
 
@@ -12,7 +12,7 @@ if (isset($_SESSION['usr'])) { ?>
     </div>
 
 
-	<?php 
+	<?php
 }
 else {//NOT LOGGED IN OVERVIEW ?>
 
@@ -26,7 +26,7 @@ else {//NOT LOGGED IN OVERVIEW ?>
 
 
         <div class="maintext">
-        
+
         <?php
         //include('generator_oneheadingmultipleimages.php');
         ?>
@@ -51,28 +51,28 @@ else {//NOT LOGGED IN OVERVIEW ?>
         $exceptions = array('.', '..');
         $bscmscexceptions = array('.', '..');
         $hiddenIndicators = array('#', '-', '_', '.');
-        
+
         $path_base = './.our_horses';
         //$path_base = '.';
         $horses = scandir($path_base);
         $printed_already_once = array();
-        
+
         // construct arrays for sorting categories (as specified in the file/directory names:
 	    $categories_pre_indices = array();
 	    $categories_pre = array();
 	    $categories_pre_index = 0;
-	    
+
 	    $categories = array();
 	    $categories_index = -1;
-        
+
 	    $categories_succeding_indices = array();
 	    $categories_succeding = array();
 	    $categories_succeding_index = 0;
-        
+
 	    $no_category_entries = array();
 
         $categories_to_order_index = array();
-	    
+
         foreach ($horses as $horseline) {
             //skip this entry?
             if ($horseline == $_SERVER['PHP_SELF']
@@ -80,7 +80,7 @@ else {//NOT LOGGED IN OVERVIEW ?>
                     || array_search(substr($horseline, 0, 1), $hiddenIndicators) !== false) {
                 continue;
             }
-            
+
             //parse the horse line data
             $parts = explode('__', $horseline);
 			/*if (count($parts) < 1)
@@ -103,8 +103,8 @@ else {//NOT LOGGED IN OVERVIEW ?>
             /*sort the elements to unify the order - so that it's not important in which order the pairs are in the directory name
             and hence element pairs simply can be omitted!*/
             //asort($elements);
-            
-            
+
+
             $entry = '<hr />';
             //Horse image heading (one heading has multiple images underneath):
             $entry .= '<p>';
@@ -128,10 +128,10 @@ else {//NOT LOGGED IN OVERVIEW ?>
                 $is_bold = strpos($key, '.bold');
                 $is_name = strpos($key, 'name');
                 if ($is_bold !== false || $is_name !== false) {
-                    $horseline_render .= 
+                    $horseline_render .=
                     /*echo*/ '<strong>';
                 }
-                
+
                 //special output:
                 if ($key == 'cat' || $key == 'category')
                     continue;
@@ -139,45 +139,45 @@ else {//NOT LOGGED IN OVERVIEW ?>
                 else if ($key == 'geburt') {
                     $age = $age_reference - $horse['geburt']/*or $value*/;
 					//echo 'AGE = ' . $age;
-                    $horseline_render .= 
+                    $horseline_render .=
                     /*echo*/ $age . ' Jahre';
                 }
                 else if ($key == 'stockmass') {
-                    $horseline_render .= 
+                    $horseline_render .=
                     /*echo*/  str_replace('_', ',', $value) . ' Stockma&szlig;';/*1_10m -> 1,10m*/
                 }
                 else if ($key == 'gegangen' || $key == 'gone' || $key == 'tod' || $key == 'Tod' || strpos($key, '.force') !== false) {
-                    $horseline_render .= 
+                    $horseline_render .=
                     /*echo*/  toFairy(str_replace('.force', '', $key)) . ': ' . toFairy($value);/*1_10m -> 1,10m*/
                 }
                 // general case:
                 else {
                     $horseline_render .= /*echo*/ /*ucfirst($key) . */ toFairy($value);
                 }
-                
-                
+
+
                 // separator
                 if ($is_bold !== false || $is_name !== false) {
-                    $horseline_render .= 
+                    $horseline_render .=
                     /*echo*/ '</strong>';
                 }
                 // Is this the last element pair?
                 if (++$i != $horseL/* - 1*/) {
                     if ($is_name !== false) {
-                        $horseline_render .= 
+                        $horseline_render .=
                         /*echo*/ ' ~ ';
                     }
                     else {
-                        $horseline_render .= 
+                        $horseline_render .=
                         /*echo*/ ', ';
                     }
                 }
             }
             $entry .= $horseline_render . "\n";
             $entry .= '<p>';
-            
-            
-            
+
+
+
             // SHOW ALL IMAGES THAT ARE FOUND IN THE DIRECTORY:
             $directory = $path_base . '/' . $horseline;
 			$horse_images = array();
@@ -202,12 +202,12 @@ else {//NOT LOGGED IN OVERVIEW ?>
                         || array_search(substr($file, 0, 1), $hiddenIndicators) !== false) {
                     continue;
                 }
-                
+
                 //assemble the imagelinks:
                 $prefix = '';
                 if (!empty($directory)) # It is empty e.g. if the images array contains the directory itself because it's a file instead of a directory.
-					$prefix = $directory . '/'; 
-					
+					$prefix = $directory . '/';
+
                 $imagelink = $prefix . $file;
                 $previewlink = $prefix . '.preview__' . $file;
                 #$previewlink =  str_replace('.jpg', '', $imagelink, . '__preview.jpg';
@@ -228,14 +228,14 @@ else {//NOT LOGGED IN OVERVIEW ?>
                             </a>
 		                </span>
 		                ';
-		        
+
             }
-            
+
     	    $entry .= '
                     <br style="clear:both;"><!-- Clear columns. Begin new line. --><p>&nbsp;</p>
 	            	';
-		    
-            
+
+
             // store if printing heading once
             if (isset($horse['cat'])) {
 				// order index is appended with .:
@@ -248,7 +248,7 @@ else {//NOT LOGGED IN OVERVIEW ?>
 					$cat = $parts[0];
 					$categories_to_order_index[$cat] = $parts[1];
 				}
-                
+
 				// At this point $cat is set correctly no matter if order index has initially been appended or not.
 				if (!isset($categories_to_order_index[$cat]))
 				{
@@ -272,7 +272,7 @@ else {//NOT LOGGED IN OVERVIEW ?>
                             $categories_succeding_indices[$categories_succeding_index] = $cat_order_index;
 					    }
 					    $categories_succeding[$categories_succeding_index][$cat][] = $entry;
-                        
+
 				    }
 				    else // cat_order_index valid and not succeding:
 				    {
@@ -288,9 +288,9 @@ else {//NOT LOGGED IN OVERVIEW ?>
                         }
 				        $categories_pre[$categories_pre_index][$cat][] = $entry;
 				        //echo '<br/>SETTING CATEGORIES PRE: ' . $cat . ' to entry: ' .$entry;
-				        
+
 				    }
-                    
+
 				}
 				// Ensure proper category (without a potential temporary order index)
 				//aready ensured above with initializing it and then overwriting$horse['cat'] = $cat;
@@ -301,14 +301,14 @@ else {//NOT LOGGED IN OVERVIEW ?>
 	    	    // No category defined, store it nevertheless, add first to make it visible to find anomalies immediately
 	    	    //  and because this way there is no impression that these category-less entries belong to the last heading.
                 $no_category_entries[] = $entry;
-	    	}    
-			
+	    	}
+
 		}
-        
-        
+
+
         // OUTPUT
 		process_entries($no_category_entries, '', $printed_already_once);
-		
+
         // sort categories according to specified position using the indices array:
         array_multisort($categories_pre_indices, $categories_pre, SORT_ASC, SORT_REGULAR);
         /*
@@ -336,23 +336,23 @@ else {//NOT LOGGED IN OVERVIEW ?>
                 process_entries($entries, $cat, $printed_already_once);
 	    	}
 
-		
+
 		?>
 
 		</div>
-		
-		
-		
-	</div>	
-</div>	
-	
+
+
+
+	</div>
+</div>
+
 <?php }//NOT LOGGED IN OVERVIEW -END
 function process_entries($entries, $cat, $printed_already_once)
 {
             //echo 'entries: ';
             //var_dump($entries);
             //echo 'category: ';
-            //var_dump($cat);           
+            //var_dump($cat);
  	//print heading once
            	if (isset($cat) && !empty($cat)) {
            		if (!isset($printed_already_once[$cat])) {
@@ -362,10 +362,10 @@ function process_entries($entries, $cat, $printed_already_once)
                         // remove spaces
 				        $post = trim(strtolower($post));
                     }
-                    
+
            		    echo '<h3>Unsere ' . toFairy($cat) . $post . ':</h3>';
                    	$printed_already_once[$cat] = true;
-                       
+
                    	// Print special notes:
                    	if ($cat == 'hof') {
                    		echo '<p>Alle unsere Schulpferde werden regelm&auml;&szlig;ig von fachkundigem Reiter korrigiert und weiter ausgebildet.</p>';
@@ -373,7 +373,7 @@ function process_entries($entries, $cat, $printed_already_once)
                	}
               	//unset($cat);
            	}
-           
+
     		foreach ($entries as $entry)
 	    	{
             	echo $entry;
